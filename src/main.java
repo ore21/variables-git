703cddef176f3a9d8dcabe1b1b0e3c29262a21e7
Brandon <bgonzalez526@students.mchenry.edu>
2018-08-30 10:24:20 -0500

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,21 @@
+public class main {
+    public static void main(String[] args){
+        byte by = 8;
+        short shrt = 4;
+        int i = 32;
+        long lg = 365345567;
+        float myFloat = 5.46f;
+        double db = 29.57;
+        boolean bln = false;
+        char ch = 'm';
+
+        System.out.println(" Byte: " + by);
+        System.out.println(" Short: " + shrt);
+        System.out.println(" Int: " + i);
+        System.out.println(" Long: " + lg);
+        System.out.println(" Float: " + myFloat);
+        System.out.println(" Double: " + db);
+        System.out.println(" Boolean: " + bln);
+        System.out.println(" Char: " + ch);
+    }
+}
